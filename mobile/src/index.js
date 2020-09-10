0e402d8538dc447610d8e8c3a1170e2f7f211422
Renan Oliveira da Silva <renanosilva@gmail.com>
2020-09-09 23:29:09 -0300

    A few differences between JS and Native

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, StatusBar  } from 'react-native';
 
 export default function App(){
-    return <View>
-        <Text>Hello</Text>
-    </View>;
+    return (
+        <>
+            <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
+            <View style={styles.container}>
+                <Text style={styles.title}>Hello</Text>
+            </View>
+        </>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#7159c1',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    title: {
+        color: '#FFF',
+        fontSize: 33,
+        fontWeight: 'bold'
+    }
+});
